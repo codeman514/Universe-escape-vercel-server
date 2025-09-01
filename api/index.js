dee043dd3a517affd9fab7ea4bec43304d09ddc0
codeman514 <gmail>
2025-09-01 18:27:18 +0800

    +移除不必要suucess回傳資料

--- api/index.js
+++ api/index.js
@@ -10,8 +10,8 @@ export default async function handler(req, res) {
   try {
     const {query} = req.body;
     const result = await pool.query(query);
-    res.status(200).json({ success: true, data: result.rows });
+    res.status(200).json({ data: result.rows });
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
